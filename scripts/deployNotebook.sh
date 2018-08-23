bc735125eac0b35df7d57c43d134d03d1871ee70
dhirajdc <dhirajc@zimetrics.com>
2018-08-23 19:07:08 +0530

    Update deployNotebook.sh

--- scripts/deployNotebook.sh
+++ scripts/deployNotebook.sh
@@ -1,9 +1,10 @@
-curl -XPOST -d @jk_prim_1_1.json http://127.0.0.1:8080/api/notebook/import > out.txt
-noteid=`cat out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
-echo "uov_prim_1_1.sh,$noteid" > params.txt
-curl -XPOST -d @jk_test_1_1.json http://127.0.0.1:8080/api/notebook/import > out.txt
-noteid=`cat out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
-echo "uov_test_1_1.sh,$noteid" >> params.txt
-curl -XPOST -d @jk_comparator_1_1.json http://127.0.0.1:8080/api/notebook/import > out.txt
-noteid=`cat out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
-echo "comparator_1_1.sh,$noteid" >> params.txt
+FPATH="/home/mdt-worker/poc/work"
+curl -XPOST -d @${FPATH}/jk_prim_1_1.json http://127.0.0.1:8080/api/notebook/import > ${FPATH}/out.txt
+noteid=`cat ${FPATH}/out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
+echo "uov_prim_1_1.sh,$noteid" > ${FPATH}/params.txt
+curl -XPOST -d @${FPATH}/jk_test_1_1.json http://127.0.0.1:8080/api/notebook/import > ${FPATH}/out.txt
+noteid=`cat ${FPATH}/out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
+echo "uov_test_1_1.sh,$noteid" >> ${FPATH}/params.txt
+curl -XPOST -d @${FPATH}/jk_comparator_1_1.json http://127.0.0.1:8080/api/notebook/import > ${FPATH}/out.txt
+noteid=`cat ${FPATH}/out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
+echo "comparator_1_1.sh,$noteid" >> ${FPATH}/params.txt
