90b9c20d7ee26fe9b4c3be4addd76a27c646c85f
dhirajdc <dhirajc@zimetrics.com>
2018-08-23 20:25:52 +0530

    Update deployNotebook.sh

--- scripts/deployNotebook.sh
+++ scripts/deployNotebook.sh
@@ -10,7 +10,7 @@ noteid=`cat ${FPATH}/out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\
 echo "comparator_1_1.sh,$noteid" >> ${FPATH}/params.txt
 curl -XPOST -d @${FPATH}/jk_prim_1_2.json http://127.0.0.1:8080/api/notebook/import > ${FPATH}/out.txt
 noteid=`cat ${FPATH}/out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
-echo "uov_prim_1_2.sh,$noteid" > ${FPATH}/params.txt
+echo "uov_prim_1_2.sh,$noteid" >> ${FPATH}/params.txt
 curl -XPOST -d @${FPATH}/jk_test_1_2.json http://127.0.0.1:8080/api/notebook/import > ${FPATH}/out.txt
 noteid=`cat ${FPATH}/out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
 echo "uov_test_1_2.sh,$noteid" >> ${FPATH}/params.txt
