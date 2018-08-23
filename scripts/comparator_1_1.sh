d3869b7da79b276f9dd329783e32946dd8329227
dhirajdc <dhirajc@zimetrics.com>
2018-08-23 14:57:52 +0530

    Update comparator_1_1.sh

--- scripts/comparator_1_1.sh
+++ scripts/comparator_1_1.sh
@@ -1,1 +1,3 @@
-curl -XPOST http://127.0.0.1:8090/api/notebook/job/2DNVNPZ8C
+fname=`basename $0`
+var=`grep $fname params.txt | cut -d, -f2`
+curl -XPOST http://127.0.0.1:8090/api/notebook/job/${var}
