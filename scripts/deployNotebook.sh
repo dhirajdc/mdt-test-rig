a3c5812ce13ca0a3b2478b82917748086767485b
dhirajdc <dhirajc@zimetrics.com>
2018-10-31 18:16:29 +0530

    Update deployNotebook.sh

--- scripts/deployNotebook.sh
+++ scripts/deployNotebook.sh
@@ -1,10 +1,10 @@
 FPATH="/home/mdt-worker/poc/work"
 
-curl -XPOST -d @${FPATH}/Uov_Comparator_1.json http://127.0.0.1:8080/api/notebook/import > ${FPATH}/out.txt
+curl -XPOST -d @${FPATH}/Uov_Comparator_1.json http://127.0.0.1:9010/api/notebook/import > ${FPATH}/out.txt
 noteid=`cat ${FPATH}/out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
 echo "Uov_Comparator_1.sh,$noteid" >> ${FPATH}/params.txt
 
 
-curl -XPOST -d @${FPATH}/Uov_Comparator_2.json http://127.0.0.1:8080/api/notebook/import > ${FPATH}/out.txt
+curl -XPOST -d @${FPATH}/Uov_Comparator_2.json http://127.0.0.1:9010/api/notebook/import > ${FPATH}/out.txt
 noteid=`cat ${FPATH}/out.txt | cut -d, -f3 | cut -d: -f2 | cut -d} -f1 | cut -d\" -f2`
 echo "Uov_Comparator_2.sh,$noteid" >> ${FPATH}/params.txt
